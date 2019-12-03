4fdbe3668e2f0f7ea047cd4d07d26bd26a02d79d
Hannes Nevalainen <hannes.nevalainen@apsis.com>
2019-12-03 08:04:31 +0100

    part 1

--- 03/03.exs
+++ 03/03.exs
@@ -1,30 +1,42 @@
 defmodule AoC2019.Day3 do
   def part1() do
-    line1 =
-      parse_offsets(~w[R8 U5 L5 D3])
-      |> line_segments({0, 0})
-      |> IO.inspect(label: "segments")
+    [path] = System.argv()
 
-    line2 =
-      parse_offsets(~w[U7 R6 D4 L4])
-      |> line_segments({0, 0})
-      |> IO.inspect(label: "segments")
+    [line1, line2 | _] =
+      File.read!(path)
+      |> String.split("\n")
+      |> Enum.map(&parse_lines/1)
+      |> IO.inspect(label: "input")
 
     intersections(line1, line2)
-    |> IO.inspect(label: "result")
+    |> IO.inspect(label: "intersections")
+    |> Enum.map(&distance/1)
+    |> Enum.min()
+    |> IO.inspect(label: "part1")
+  end
+
+  defp parse_lines(input) do
+    input
+    |> String.split(",")
+    |> parse_offsets()
+    |> line_segments({0, 0})
   end
 
   defp line_segments([], _pos), do: []
 
   defp line_segments([p | rest], pos) do
     new_pos = move(pos, p)
-    [{pos, new_pos} | line_segments(rest, new_pos)]
+    [line_segment(pos, new_pos) | line_segments(rest, new_pos)]
   end
 
   defp move({x, y}, {"R", dx}), do: {x + dx, y}
   defp move({x, y}, {"L", dx}), do: {x - dx, y}
   defp move({x, y}, {"U", dy}), do: {x, y + dy}
   defp move({x, y}, {"D", dy}), do: {x, y - dy}
+
+  defp line_segment({x, y}, {u, y}) when x > u, do: {{u, y}, {x, y}}
+  defp line_segment({x, y}, {x, v}) when y > v, do: {{x, v}, {x, y}}
+  defp line_segment(p1, p2), do: {p1, p2}
 
   defp parse_offsets([]), do: []
 
@@ -34,34 +46,30 @@ defmodule AoC2019.Day3 do
   end
 
   defp intersections(line1, line2) do
-    for l1 <- line1, l2 <- line2 do
-      intersects?(l1, l2)
-    end
+    for(l1 <- line1, l2 <- line2, do: {l1, l2})
+    |> Enum.map(fn {l1, l2} -> intersection(l1, l2) end)
+    |> Enum.reject(fn
+      {0, 0} -> true
+      :none -> true
+      _ -> false
+    end)
   end
 
-  defp vertical?({{x, _}, {x, _}}), do: true
-  defp vertical?(_), do: false
-  defp horizontal?({{_, y}, {_, y}}), do: true
-  defp horizontal?(_), do: false
+  defp distance({x, y}), do: abs(x) + abs(y)
 
-  defp intersects?(line1, line2) do
-    can_cross = horizontal?(line1) |> xor(horizontal?(line2))
-
-    if can_cross do
-      {:maybe, line1, line2}
-    else
-      false
-    end
+  defp intersection({{x1, y1}, {x2, y1}}, {{x3, y3}, {x3, y4}})
+       when x3 >= x1 and x3 <= x2 and y1 >= y3 and y1 <= y4 do
+    {x3, y1}
   end
 
-  defp xor(p1, p2) do
-    (p1 && p2) || (!p1 && !p2)
+  defp intersection({{x1, y1}, {x1, y2}}, {{x3, y3}, {x4, y3}})
+       when y3 >= y1 and y3 <= y2 and x1 >= x3 and x1 <= x4 do
+    {x1, y3}
   end
 
-  defp s({start_point, _end_point}), do: start_point
-  defp e({_start_point, end_point}), do: end_point
-  defp x({x, _}), do: x
-  defp y({_, y}), do: y
+  defp intersection(_, _) do
+    :none
+  end
 end
 
 AoC2019.Day3.part1()
