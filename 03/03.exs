6523312f3cc02bf02050fdb5824f7cac400984bc
Hannes Nevalainen <hannes.nevalainen@me.com>
2019-12-03 22:42:09 +0100

    part 2

--- 03/03.exs
+++ 03/03.exs
@@ -6,13 +6,69 @@ defmodule AoC2019.Day3 do
       File.read!(path)
       |> String.split("\n")
       |> Enum.map(&parse_lines/1)
-      |> IO.inspect(label: "input")
 
     intersections(line1, line2)
-    |> IO.inspect(label: "intersections")
     |> Enum.map(&distance/1)
     |> Enum.min()
     |> IO.inspect(label: "part1")
+  end
+
+  def part2() do
+    [path] = System.argv()
+
+    [line1, line2 | _] =
+      File.read!(path)
+      |> String.split("\n")
+      |> Enum.map(&parse_instructions/1)
+
+    calc(line1, line2)
+    |> MapSet.new()
+    |> MapSet.intersection(calc(line2, line1) |> MapSet.new())
+    |> Enum.sort()
+    |> IO.inspect()
+  end
+
+  defp calc(line1, line2) do
+    %{}
+    |> paint_map({0, 0}, 0, line2, fn cost, new_cost -> min(cost, new_cost) end)
+    |> paint_map({0, 0}, 0, line1, fn cost, new_cost -> {:intersect, cost + new_cost} end)
+    |> Map.delete({0, 0})
+    |> Enum.filter(&match?({_, {:intersect, n}}, &1))
+    |> Enum.map(fn {key, value} -> {value, key} end)
+    |> Enum.sort()
+    |> IO.inspect(label: "part2")
+  end
+
+  defp paint_map(map, _, _, [], fun), do: map
+
+  defp paint_map(map, pos, cost, [{_, 0} | rest], fun), do: paint_map(map, pos, cost, rest, fun)
+
+  defp paint_map(map, pos, cost, [{dir, n} | rest], fun) do
+    map
+    |> Map.update(pos, cost, fn old_cost -> fun.(old_cost, cost) end)
+    |> paint_map(translate(pos, dir), cost + 1, [{dir, n - 1} | rest], fun)
+  end
+
+  defp translate({x, y}, {dx, dy}) do
+    {x + dx, y + dy}
+  end
+
+  def parse_instructions(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(&parse_instruction/1)
+  end
+
+  def parse_instruction(input) do
+    {dir, amount} = String.split_at(input, 1)
+    amount = String.to_integer(amount)
+
+    case dir do
+      "D" -> {{0, -1}, amount}
+      "U" -> {{0, 1}, amount}
+      "L" -> {{-1, 0}, amount}
+      "R" -> {{1, 0}, amount}
+    end
   end
 
   defp parse_lines(input) do
@@ -71,3 +127,4 @@ defmodule AoC2019.Day3 do
 end
 
 AoC2019.Day3.part1()
+AoC2019.Day3.part2()
